1349b0fc62c7c8825a120bcd9f8f65cd01a689d1
Jin Dou <879432276@qq.com>
2019-02-24 14:57:42 -0800

    Update cRouter.cpp

--- cRouter.cpp
+++ cRouter.cpp
@@ -61,9 +61,12 @@ string flow_table::insert(octane_control msg)
 		cout << "flow_table_insert warning: replace existed extry";
 	}
 	m_mTable[entry] = action;
+	struct in_addr src1, dst1;
+	src1.s_addr = entry.m_srcIp;
+	dst1.s_addr = entry.m_dstIp;
 	string output = ", rule installed (" + 
-		string(inet_ntoa(entry.m_srcIp)) + ", " + to_string(int(inet_ntohs(entry.m_srcPort))) + ", " +
-		string(inet_ntoa(entry.m_dstIp)) + ", " + to_string(int(inet_ntohs(entry.m_dstPort)))+ ", " + to_string(entry.m_protocol) +
+		string(inet_ntoa(src1)) + ", " + to_string(int(inet_ntohs(entry.m_srcPort))) + ", " +
+		string(inet_ntoa(dst1)) + ", " + to_string(int(inet_ntohs(entry.m_dstPort)))+ ", " + to_string(entry.m_protocol) +
 		") action " + to_string(action.m_action);
 	return output;
 }
@@ -85,13 +88,18 @@ vector<string> flow_table::dbInsert(octane_control msg, uint16_t newFwdPort = -1
 	}
 	m_mTable[entryRev] = actionRev;
 	vector<string> log;
+	struct in_addr src1, src2, dst1, dst2;
+	src1.s_addr = entry.m_srcIp;
+	src2.s_addr = entryRev.m_srcIp;
+	dst1.s_addr = entry.m_dstIp;
+	dst2.s_addr = entryRev.m_dstIp;
 	string output1 = ", rule installed (" +
-		string(inet_ntoa(entry.m_srcIp)) + ", " + to_string(int(inet_ntohs(entry.m_srcPort))) + ", " +
-		string(inet_ntoa(entry.m_dstIp)) + ", " + to_string(int(inet_ntohs(entry.m_dstPort))) + ", " + to_string(entry.m_protocol) +
+		string(inet_ntoa(src1)) + ", " + to_string(int(inet_ntohs(entry.m_srcPort))) + ", " +
+		string(inet_ntoa(dst1)) + ", " + to_string(int(inet_ntohs(entry.m_dstPort))) + ", " + to_string(entry.m_protocol) +
 		") action " + to_string(action.m_action) ;
 	string output2 = ", rule installed (" +
-		string(inet_ntoa(entryRev.m_srcIp)) + ", " + to_string(int(inet_ntohs(entryRev.m_srcPort))) + ", " +
-		string(inet_ntoa(entryRev.m_dstIp)) + ", " + to_string(int(inet_ntohs(entryRev.m_dstPort))) + ", " + to_string(entryRev.m_protocol) +
+		string(inet_ntoa(src2)) + ", " + to_string(int(inet_ntohs(entryRev.m_srcPort))) + ", " +
+		string(inet_ntoa(dst2)) + ", " + to_string(int(inet_ntohs(entryRev.m_dstPort))) + ", " + to_string(entryRev.m_protocol) +
 		") action " + to_string(actionRev.m_action);
 	log.push_back(output1);
 	log.push_back(output2);

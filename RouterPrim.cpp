0421efec852dcd695715ffa3a849780cbf47e3c9
Jin Dou <879432276@qq.com>
2019-02-28 11:13:57 -0800

    Update RouterPrim.cpp

--- RouterPrim.cpp
+++ RouterPrim.cpp
@@ -199,10 +199,24 @@ void primaryRouter_s4(cRouter & Router, sockaddr_in &rou2Addr)
 				else
 				{
 					printf("Read a packet from secondary router, packet length:%d\n", nread);
-					icmpForward_log(Router, buffer, sizeof(buffer), FromUdp, ntohs(rou2Addr.sin_port));
-					cwrite(tun_fd, buffer, nread);// send packet back to tunnel
-												  //sendMsg(Router.iSockID, buffer, sizeof(buffer), rou1Addr);
-												  //icmpReply_primRouter(tun_fd, buffer, nread);
+					int iIcmpProtocol = icmpForward_log(Router, buffer, sizeof(buffer), FromUdp, ntohs(rou2Addr.sin_port));
+					if (iIcmpProtocol == 1)// its a icmp pscket
+					{
+						cwrite(tun_fd, buffer, nread);// send packet back to tunnel
+													  //sendMsg(Router.iSockID, buffer, sizeof(buffer), rou1Addr);
+													  //icmpReply_primRouter(tun_fd, buffer, nread);
+					}
+					else if (iIcmpProtocol == OCTANE_PROTOCOL_NUM)
+					{
+						// check seqno and remove related record from the unack_buffer
+						octane_control octMsg;
+						uint16_t iSeqno = octaneUnpack(buffer, &octMsg);
+						if (octMsg.octane_flags == 1)
+						{
+							Router.m_unAckBuffer.erase(iSeqno);
+							Router.printUnAckBuffer();
+						}
+					}
 				}
 			}
 			timeout.tv_sec = 15;

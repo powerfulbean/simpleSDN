163fd7f53fe7704f5e2b11b076de6a1dd91dc17d
Jin Dou <879432276@qq.com>
2019-04-13 15:36:54 -0700

    Update cmd_stage9.sh

--- cmd_stage9.sh
+++ cmd_stage9.sh
@@ -1,2 +1,2 @@
-sudo iptables -t nat -A OUTPUT -s 192.168.203.2 -p tcp --dport 80 -j DNAT \ --to-destination 127.0.0.1:8080
+sudo iptables -t nat -A OUTPUT -s 192.168.203.2 -p tcp --dport 80 -j DNAT --to-destination 127.0.0.1:8080
 

9899b1dec056fd31b274b0e6b9cd151992671808
Jin Dou <879432276@qq.com>
2019-04-15 22:13:30 -0700

    Update cRouter.cpp

--- cRouter.cpp
+++ cRouter.cpp
@@ -53,8 +53,11 @@ bool flow_entry::operator< (const flow_entry key2) const
 {
 	if (m_srcIp == htonl(0xFFFFFFFF) || m_srcPort == htons(0xFFFF) || m_dstIp == htonl(0xFFFFFFFF) || m_dstPort == htons(0xFFFF))
 	{
+		struct in_addr src1, dst1;
+		src1.s_addr = m_srcIp;
+		dst1.s_addr = m_dstIp;
 		cout << "hit some FFFF rules!!!!!!!!!!!!";
-		printf("%x %x %x %x\n", m_srcIp, m_srcPort, m_dstIp, m_dstPort);
+		printf("%d %d %d %d\n", inet_ntoa(src1), ntohs(m_srcPort), inet_ntoa(dst1), ntohs(m_dstPort));
 
 	}
 	if (m_srcIp != key2.m_srcIp  && m_srcIp != htonl(0xFFFFFFFF) )

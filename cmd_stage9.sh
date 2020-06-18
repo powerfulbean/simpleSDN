97a14a9a80fa2a2061ea3a745a16b052709ff3e5
powerfulbean <samwisedou@outlook.com>
2020-06-17 19:55:45 -0700

    Update

--- cmd_stage9.sh
+++
@@ -1,2 +0,0 @@
-sudo iptables -t nat -A OUTPUT -s 192.168.203.2 -p tcp --dport 80 -j DNAT --to-destination 127.0.0.1:80
-
--- stage10Exe.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash 
-for i in {0..1}; do 
-  curl --local-port $(($i+50009)) --interface eth0 -X GET 'https://pdos.csail.mit.edu'
-done
-
